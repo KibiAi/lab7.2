4ed97861100cad4c62cdf9ed5706edad602986d5
KibiAi <vetal2004v@gmail.com>
2023-11-06 13:27:45 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using lab7._2;
+
+class Program
+{
+    static void Main()
+    {
+        Repository<string> stringRepository = new Repository<string>();
+
+        stringRepository.Add("Dog");
+        stringRepository.Add("Snake");
+        stringRepository.Add("Cat");
+        stringRepository.Add("Crocodile");
+
+        Repository<string>.Criteria<string> criteria = s => s.Length > 3;
+
+        List<string> longWords = stringRepository.Find(criteria);
+
+        foreach (string word in longWords)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
